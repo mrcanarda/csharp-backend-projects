68990b68f410ba951138a23ccffa12d42943f332
Can Arda <canarda.dev@gmail.com>
2026-04-09 22:38:26 +0200

    feat: async/await, Task - day 7

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9041e2f104860f2c4f45fea0fcca81f3a0483b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6c7e100d3212c65aad120a2350c4a74c5d661a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
